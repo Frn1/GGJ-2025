12f6352df33b787cb1e0dad99290afb30886765b
Frn1 <torrescontarini@gmail.com>
2025-01-23 20:33:13 -0300

    Add bubble spawn object (without code)

---
+++ objects/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export_range(0, 1) var player = 0 
+
+@export var speed = 300.0
+@export var jump_velocity = 400.0
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("jump_p" + str(player)) and is_on_floor():
+		velocity.y = -jump_velocity
+	
+	var direction := Input.get_axis("move_left_p" + str(player), "move_right_p" + str(player))
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	move_and_slide()
