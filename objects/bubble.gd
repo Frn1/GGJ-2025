c49febc1076c83d237b7b103c0dfb75d1fa00749
Frn1 <torrescontarini@gmail.com>
2025-01-23 20:51:46 -0300

    Add bubble collecting

--- objects/bubble.gd
+++ objects/bubble.gd
@@ -1,4 +1,6 @@
 extends Area2D
 
-func _on_area_entered(area: Area2D) -> void:
-	print("Colision con ", area.get_overlapping_bodies()[0])
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.score += 1
+		queue_free()
--- objects/player.gd
+++ objects/player.gd
@@ -1,9 +1,13 @@
+class_name Player
+
 extends CharacterBody2D
 
 @export_range(0, 1) var player = 0 
 
 @export var speed = 300.0
 @export var jump_velocity = 400.0
+
+@export var score: int = 0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
