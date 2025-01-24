ac795fef0aa086123354a6e857f8a14401cc6a90
Frn1 <torrescontarini@gmail.com>
2025-01-23 21:08:49 -0300

    Add player spawning

--- objects/player.gd
+++ objects/player.gd
@@ -2,7 +2,7 @@ class_name Player
 
 extends CharacterBody2D
 
-@export_range(0, 1) var player = 0 
+@export_range(0, 1) var number = 0 
 
 @export var speed = 300.0
 @export var jump_velocity = 400.0
@@ -13,10 +13,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if Input.is_action_just_pressed("jump_p" + str(player)) and is_on_floor():
+	if Input.is_action_just_pressed("jump_p" + str(number)) and is_on_floor():
 		velocity.y = -jump_velocity
 	
-	var direction := Input.get_axis("move_left_p" + str(player), "move_right_p" + str(player))
+	var direction := Input.get_axis("move_left_p" + str(number), "move_right_p" + str(number))
 	if direction:
 		velocity.x = direction * speed
 	else:
--- scenes/game.gd
+++ scenes/game.gd
@@ -1,13 +1,25 @@
 extends Node2D
 
 @export var level_scene: PackedScene
+@export var player_scene: PackedScene = preload("res://objects/player.tscn")
 
 var level: Level
 @onready var camera: Camera2D = $Camera
 
 var spawns: Array = []
 
+func spawn_player(number: int) -> void:
+	const PLAYER_SPAWN_SOURCE_ID: int = 0
+	var spawn_coords = level.meta.get_used_cells_by_id(PLAYER_SPAWN_SOURCE_ID, Vector2i(-1, -1), number)
+	for coord in spawn_coords:
+		var player: Player = player_scene.instantiate()
+		var pos = level.meta.map_to_local(coord)
+		player.number = number
+		player.position = pos
+		add_child(player)
+
 func _ready() -> void:
 	level = level_scene.instantiate()
 	add_child(level)
-	
+	spawn_player(0)
+	print()
