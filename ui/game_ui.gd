a734426c39aa2d28383a61d8e835ad96f01a53a1
Frn1 <torrescontarini@gmail.com>
2025-01-26 23:33:59 -0300

    Add UIs

--- ui/game_ui.gd
+++ ui/game_ui.gd
@@ -1,4 +1,4 @@
 extends Control
 
-@onready var player_0: Label = $Scores/HBoxContainer/Player0 
-@onready var player_1: Label = $Scores/HBoxContainer/Player1
+@onready var player_0: Label = $Player0/Control/Score
+@onready var player_1: Label = $Player0/Control/Score
