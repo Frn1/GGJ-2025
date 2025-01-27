dcbec4308471128824f2044bb62a381227308499
Frn1 <torrescontarini@gmail.com>
2025-01-27 04:36:32 -0300

    Hide meta objects when necessary

--- objects/meta/bubble_spawn.gd
+++ objects/meta/bubble_spawn.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 func _ready() -> void:
-	if OS.is_debug_build():
+	if OS.is_debug_build() and OS.has_feature("movie") == false:
 		visible = true
-	else: 
+	else:
 		visible = false
 		for child in get_children():
 			child.queue_free()
--- objects/meta/player_spawn.gd
+++ objects/meta/player_spawn.gd
@@ -3,3 +3,11 @@ class_name PlayerSpawn
 extends Node2D
 
 @export_range(0, 1) var player: int = 0
+
+func _ready() -> void:
+	if OS.is_debug_build() and OS.has_feature("movie") == false:
+		visible = true
+	else:
+		visible = false
+		for child in get_children():
+			child.queue_free()
